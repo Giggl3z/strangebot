c0ee5f4f4313a640d7c271a09898a75187d46ac8
Giggl3z <35525972+Giggl3z@users.noreply.github.com>
2019-04-16 04:07:15 -0400

    Update lol.js

--- lol.js
+++ lol.js
@@ -1,26 +1,8 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const fs = require("fs");
-bot.commands = new Discord.Collection();
 
-fs.readdir("./commands/", (err, files) => {
-    if (err)
-    {
-        console.log(err);
-    }
-    let jsfile = files.filter(f => f.split(".").pop() === "js")
-    if (jsfile.length <= 0)
-    {
-        console.log("Couldn't find commands");
-        return;
-    }
 
-    jsfile.forEach((f, i) => {
-        let props = require(`./commands/${f}`);
-        console.log(`${f} loaded!`);
-        bot.commands.set(props.help.name, props);
-    });
-});
 
 function randint(min, max) {
     return Math.floor(min + Math.random()*(max + 1 - min));
@@ -80,12 +62,6 @@ bot.on("message", message => {
         let cmd = messageArray[0];
         let prefix = ".s ";
         let args = messageArray.slice(1);
-
-        let commandfile = bot.commands.get(cmd.slice(prefix.length));
-        if (commandfile)
-        {
-            commandfile.run(bot, message, args);
-        }
 
         //console.log(levelUp);
         let strangePoint = randint(7, 13);
