caf90206d6090d6103741910766ca8ae307cc1e2
Giggl3z <35525972+Giggl3z@users.noreply.github.com>
2019-04-16 20:55:26 -0400

    Update lol.js

--- lol.js
+++ lol.js
@@ -178,7 +178,7 @@ bot.on("message", message => {
 
         else if (message.content.includes("osint") || message.content.includes("osintsec") || message.content.includes("Osint") || message.content.includes("Osintsec"))
         {
-            message.channel.send(`<@564474717747150858> ${message.author.username} is talking about you`);
+            message.channel.send(`<@564474717747150858> ${message.author.username} is talking about you`.toLowerCase);
         }
 
         else if (message.content.includes("thanks"))
