1f3b7cc706ffcc1a6363a1f6222d6e684e0c9050
Giggl3z <35525972+Giggl3z@users.noreply.github.com>
2019-04-17 04:19:36 -0400

    Update lol.js

--- lol.js
+++ lol.js
@@ -170,6 +170,32 @@ bot.on("message", message => {
             });
         }
 
+        if (message.content.startsWith(prefix + "eval"))
+        {
+            const clean = text => {
+                if (typeof(text) === "string")
+                    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+                else
+                    return text;
+                }
+            if(message.author.id !== 564474717747150858) return;
+
+            try
+            {
+                const code = args.join(" ");
+                let evaled = eval(code);
+
+                if (typeof evaled !== "string")
+                {
+                    evaled = require("util").inspect(evaled);
+                }
+
+                message.channel.send(clean(evaled), {code:"xl"});
+            } catch (err) {
+                message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+            }
+        }
+
         if (message.content.includes("stfu"))
         {
             message.channel.send("no u");
